bcd8aa3c9f506084c90febffc521752d07043a19
alex-wdmg <wdmg.com.ua@gmail.com>
2019-04-06 16:48:06 +0300

    Added copyrights

--- src/StopListValidator.php
+++ src/StopListValidator.php
@@ -2,6 +2,18 @@
 
 namespace wdmg\validators;
 
+/**
+ * Yii2 Stop list validator
+ *
+ * @category        Validators
+ * @version         1.0.1
+ * @author          Alexsander Vyshnyvetskyy <alex.vyshnyvetskyy@gmail.com>
+ * @link            https://github.com/wdmg/yii2-validators
+ * @copyright       Copyright (c) 2019 W.D.M.Group, Ukraine
+ * @license         https://opensource.org/licenses/MIT Massachusetts Institute of Technology (MIT) License
+ *
+ */
+
 use Yii;
 use yii\base\InvalidConfigException;
 use yii\validators\Validator;
